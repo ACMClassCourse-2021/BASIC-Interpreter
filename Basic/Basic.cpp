09c5c4bad9d89f184b6f5f6b64a59b26e64c81c1
shanjiaming <68770027+shanjiaming@users.noreply.github.com>
2021-11-13 12:40:36 +0800

    correct comment

--- Basic/Basic.cpp
+++ Basic/Basic.cpp
@@ -45,9 +45,8 @@ int main() {
  * Function: processLine
  * Usage: processLine(line, program, state);
  * -----------------------------------------
- * Processes a single line entered by the user.  In this version,
- * the implementation does exactly what the interpreter program
- * does in Chapter 19: read a line, parse it as an expression,
+ * Processes a single line entered by the user.  In this version of
+ * implementation, the program read a line, parse it as an expression,
  * and then print the result.  In your implementation, you will
  * need to replace this method with one that can respond correctly
  * when the user enters a program line (which begins with a number)
